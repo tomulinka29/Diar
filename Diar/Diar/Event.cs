0ac9a41d09ec43787198802f84d3d1fc494b8643
tomulinka29 <tomikbruna@seznam.cz>
2021-06-25 17:57:13 +0200

    Add files via upload

---
+++ Diar/Diar/Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diar
+{
+    class Event
+    {
+        public DateTime start;
+        public DateTime end;
+
+        public bool wholeDay = false;
+
+        public string name;
+        public string description;
+
+        public Event(DateTime start, DateTime end, bool wholeDay, string name, string description)
+        {
+            this.start = start;
+            this.end = end;
+            this.wholeDay = wholeDay;
+            this.name = name;
+            this.description = description;
+        }
+
+        public override string ToString()
+        {
+            return "Název: " + name + Environment.NewLine + 
+                   "Popis: " + description + Environment.NewLine + 
+                   "Začátek: " + start.ToString() + Environment.NewLine + 
+                   "Konec: " + end.ToString();
+        }
+    }
+}
---
+++ Diar/Diar/EventsManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Diar
+{
+    class EventsManager
+    {
+        private List<Event> events;
+        private string path;
+
+
+        public EventsManager(string path)
+        {
+            this.path = path;
+
+            events = new List<Event>();
+            var loaded = LoadEvents(path);
+
+            if (loaded != null)
+                events.AddRange(loaded);
+        }
+
+
+        public void RemoveEvent(Event remEvent)
+        {
+            events.Remove(remEvent);
+            SaveEvents(path);
+        }
+        public void RemoveEvent(string name) 
+        {
+            events.RemoveAll(i => i.name == name);
+            SaveEvents(path);
+        }
+        public void RemoveEvent(DateTime date)
+        {
+            events.RemoveAll(i => i.start == date);
+            SaveEvents(path);
+        }
+        public void RemoveEvents(DateTime from, DateTime to) 
+        {
+            events.RemoveAll(i => (DateTime.Compare(i.start, from) >= 0 && DateTime.Compare(i.end, to) <= 0));
+            SaveEvents(path);
+        }
+
+        public void AddEvent(Event newEvent)
+        {
+            events.Add(newEvent);
+            SaveEvents(path);
+        }
+
+
+
+        private List<Event> LoadEvents(string path)
+        {
+            try
+            {
+                List<Event> deserialized = JsonConvert.DeserializeObject<List<Event>>(File.ReadAllText(path));
+                return deserialized;
+            }
+            catch
+            {
+                Console.WriteLine("Chyba při načítání události");
+                return null;
+            }
+        }
+
+        private void SaveEvents(string path)
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(events, Formatting.Indented);
+                File.WriteAllText(path, json);
+
+            }
+            catch
+            {
+                Console.WriteLine("Chyba při ukládání události");
+            }
+        }
+
+        public List<Event> GetEvents() => events;
+    }
+}
